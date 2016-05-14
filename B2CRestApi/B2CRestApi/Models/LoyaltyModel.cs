b3b5cd8063b89d03f726d3c79bef7e6162a968de
beejones <ronny.bjones@hotmail.com>
2016-05-14 07:56:10 +0200

    Activate authorization on REST API Loyalty controller
    Activate authorization
    Cleanup

--- B2CRestApi/B2CRestApi/Models/LoyaltyModel.cs
+++ B2CRestApi/B2CRestApi/Models/LoyaltyModel.cs
@@ -5,10 +5,24 @@ using System.Web;
 
 namespace B2CRestApi.Models
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="LoyaltyModel"/> class.
+    /// </summary>
     public class LoyaltyModel
     {
+        /// <summary>
+        /// Gets or sets the id of the object
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user id of the object
+        /// </summary>
         public string UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the loyalty number
+        /// </summary>
         public string LoyaltyNumber { get; set; }
     }
 }
