817ccede86d25403096f4bc7f8b3ef50f7690417
chrispadgettlivecom <chris.padgett@live.com.au>
2016-05-17 23:59:06 -0700

    Added the home page to the ASP.NET Web API app.

--- B2CRestApi/B2CRestApi/Properties/Settings.Designer.cs
+++ B2CRestApi/B2CRestApi/Properties/Settings.Designer.cs
@@ -13,7 +13,7 @@ namespace B2CRestApi.Properties {
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+    public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
         
@@ -38,6 +38,15 @@ namespace B2CRestApi.Properties {
         public string ClientSecret {
             get {
                 return ((string)(this["ClientSecret"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("B2CCharm.onmicrosoft.com")]
+        public string B2CTenant {
+            get {
+                return ((string)(this["B2CTenant"]));
             }
         }
     }
