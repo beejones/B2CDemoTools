3697ab1616289326a6a0377f77fd839b03e0f668
chrispadgettlivecom <chris.padgett@live.com.au>
2016-05-23 09:00:42 +1000

    Added the "Register your B2C tenant" link to the home page.

--- Owin IdentityServer3/Owin IdentityServer3/IdentityServer/Controllers/HomeController.cs
+++ Owin IdentityServer3/Owin IdentityServer3/IdentityServer/Controllers/HomeController.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace IdentityServer.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
             return View();
--- Owin IdentityServer3/Owin IdentityServer3/IdentityServer/EntityFramework/CpimClientConfigurationDbContext.cs
+++ Owin IdentityServer3/Owin IdentityServer3/IdentityServer/EntityFramework/CpimClientConfigurationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using System.Data.Entity;
+﻿//using System.Data.Entity;
 using IdentityServer3.EntityFramework;
 
 namespace IdentityServer.EntityFramework
